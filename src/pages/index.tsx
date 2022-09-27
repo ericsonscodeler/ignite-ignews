2a47aed2131a1cfeb6554a21ab7590ddfc0c705e
Ericson Scodeler <ericson.br@hotmail.com>
2022-09-27 20:04:08 -0300

    backend no front end, utilizando Auth do github para logar na aplicação

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,4 +1,4 @@
-import {GetServerSideProps} from 'next'
+import {GetStaticProps} from 'next'
 import Head from 'next/head';
 import {stripe} from '../services/stripe';
 import { SubscribeButton } from '../components/SubscribeButton';
@@ -32,7 +32,7 @@ export default function Home({product}: HomeProps) {
   )
 }
 
-export const getServerSideProps: GetServerSideProps = async () => {
+export const getStaticProps: GetStaticProps = async () => {
 
   const price = await stripe.prices.retrieve(process.env.STRIPE_PRICE_ID)
 
@@ -46,6 +46,7 @@ export const getServerSideProps: GetServerSideProps = async () => {
   return {
     props: {
       product,
-    }
+    },
+    revalidate: 60 * 60 * 24, // 24 hours
   }
 }
